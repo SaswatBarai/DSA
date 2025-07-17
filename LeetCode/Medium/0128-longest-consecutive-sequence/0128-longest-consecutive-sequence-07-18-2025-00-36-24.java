63b0a6a258b8ebab61a4cb95704b997284dd5571
SASWAT BARAI <125298969+SaswatBarai@users.noreply.github.com>
2025-07-18 00:36:35 +0530

    0128-longest-consecutive-sequence 07-18-2025 Medium 32 ms (40.51%) 67.7 MB (35.35%)

---
+++ LeetCode/Medium/0128-longest-consecutive-sequence/0128-longest-consecutive-sequence-07-18-2025-00-36-24.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int longestConsecutive(int[] arr) {
+
+        int n = arr.length;
+
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int val : arr) {
+            set.add(val);
+        }
+
+        int max = 0;
+        for (int val : set) {
+            if (!set.contains(val - 1)) {
+                int x = val;
+                int count = 1;
+                while (true) {
+                    if (!set.contains(x + 1)) {
+                        break;
+                    }
+                    count++;
+                    x++;
+                }
+            max = Math.max(max, count);
+            }
+
+        }
+
+        return max;
+    }
+}
