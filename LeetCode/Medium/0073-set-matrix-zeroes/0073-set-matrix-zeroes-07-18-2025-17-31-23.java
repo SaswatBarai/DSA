dd32a66edc91ae7dd06791e5fec7d68e8d77fcf1
SASWAT BARAI <125298969+SaswatBarai@users.noreply.github.com>
2025-07-18 17:31:34 +0530

    0073-set-matrix-zeroes 07-18-2025 Medium 0 ms (100%) 45.8 MB (27.71%)

---
+++ LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes-07-18-2025-17-31-23.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean mark = false;
+
+
+        for(int i = 0; i<m; i++){
+            if(matrix[i][0] == 0) mark = true;
+
+            for(int j =1; j<n; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i = 1; i<m; i++){
+            for(int j=1; j<n; j++ ){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if(matrix[0][0] == 0){
+            for(int i=0; i<n; i++){
+                matrix[0][i] = 0;
+            }
+        }
+
+        if(mark){
+            for(int i=0; i<m; i++ ){
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
