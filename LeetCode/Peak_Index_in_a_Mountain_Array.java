6649c1552b0b3a60c89d562e4710b0f62ba17745
SASWAT BARAI <125298969+SaswatBarai@users.noreply.github.com>
2025-08-02 11:14:05 +0530

    Added LeetCode solution: Peak Index in a Mountain Array

---
+++ LeetCode/Peak_Index_in_a_Mountain_Array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        return BinarySearch(arr);
+    }
+
+
+    public static int BinarySearch(int arr[]){
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start < end ){
+            int mid = start + (end-start)/2;
+
+            if(arr[mid] < arr[mid+1]){
+                start = mid+1;
+            }
+            else if(arr[mid] > arr[mid+1]){
+                end = mid;
+            }
+        }
+
+        return start;
+    }
+}
